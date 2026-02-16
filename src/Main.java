b1e5afff4ac3b9cad8613fadad10003da3ae3fdb
pavxxx <pavithra020906@gmail.com>
2026-02-16 10:31:44 +0530

    Added extra functionality to main method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
 
     IO.println(String.format("Hello and welcome!"));
@@ -7,4 +5,28 @@ void main() {
     for (int i = 1; i <= 5; i++) {
         IO.println("i = " + i);
     }
+
+    IO.println("Enter a number:");
+    int number = IO.readInt();
+
+
+    if (number % 2 == 0) {
+        IO.println("The number is Even.");
+    } else {
+        IO.println("The number is Odd.");
+    }
+
+    int factorial = 1;
+    for (int j = 1; j <= number; j++) {
+        factorial *= j;
+    }
+
+    IO.println("Factorial of " + number + " is: " + factorial);
+
+    int sum = 0;
+    for (int k = 1; k <= number; k++) {
+        sum += k;
+    }
+
+    IO.println("Sum from 1 to " + number + " is: " + sum);
 }
